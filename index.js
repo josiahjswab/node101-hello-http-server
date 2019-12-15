99451bd9c5b2f8724006ffc2ac6ce37c57792625
Josiah Swab <josiahjswab@gmail.com>
2019-12-15 14:43:46 -0800

    Adds node http server creation

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code here
+const http = require('http');
+
+const server = http.createServer(function(req, res) {
+
+  console.log(`received ${req.method} request for: ${req.url}`);
+  
+  res.writeHead(200, {'Contenet-Type': 'text/plain'});
+
+  res.end("Hello World");
+});
+
+server.listen(8080, 'localhost', null, function(req, res) {
+
+  console.log(`Server is listening on port 8080`);
+});
+
